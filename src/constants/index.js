5256a7366a5f2ed4dddb103114f7efe253a19608
shubham maurya <sm7758525@gmail.com>
2025-08-14 01:50:17 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,92 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
+import project6 from "../assets/projects/project-6.jpg";
+
+export const HERO_CONTENT = "Passionate MERN Stack Developer skilled in MongoDB, Express.js, React.js, and Node.js. Experienced in building and deploying real-world full-stack projects with clean, responsive, and user-friendly designs. Committed to crafting scalable web solutions while continuously enhancing my skills.";
+
+export const ABOUT_TEXT = "Dedicated and versatile Full Stack Developer with strong expertise in the MERN stack. Experienced in building and deploying multiple full-stack and frontend projects using MongoDB, Express.js, React.js, Node.js, Tailwind CSS, and Bootstrap. Currently expanding my skill set with Next.js and Generative AI. Passionate about transforming ideas into responsive, user-friendly web applications, while continuously learning, adapting, and growing as a developer.";
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+   {
+    title: "AirBnb",
+    image: project1,
+    description:
+      "Wanderlust is an Airbnb-inspired travel booking app built with the MERN stack, Bootstrap, and various external packages for a responsive and feature-rich experience.",
+    technologies: ["MongoDB", "Express.js", "EJS", "Node.js", "Bootstrap", "Pasport.js"],
+  },
+  {
+    title: "YouTube Clone",
+    image: project2,
+    description:
+      "A responsive YouTube clone with features like video search, playback, and real-time data fetching using the YouTube API.",
+    technologies: ["React","Tailwind CSS","Rapid API","Vite"],
+  },
+  {
+    title: "Gemini AI Tool",
+    image: project3,
+    description:
+      "An AI-powered tool that allows users to generate Text based content and summaries using Gemini API, with a simple, responsive UI built using modern frontend technologies",
+    technologies: ["React","Tailwind CSS","Gemini API","Vite"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project4,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "Tailwind CSS","Framer Motion","Vite"],
+  },
+  {
+    title: "VirtueEdge",
+    image: project5,
+    description:
+      "A responsive and user-interactive landing page built as a practice project. It focuses on clean UI design, mobile-first responsiveness, and smooth user experience, showcasing modern frontend development skills.",
+    technologies: ["React", "Tailwind CSS", "Vite"],
+  },
+  {
+    title: "Solar System 3D",
+    image: project6,
+    description:
+      "A 3D Solar System project built using Three.js, featuring interactive planets, realistic textures, and smooth animations to simulate orbital motion—designed to enhance visual learning and 3D development skills.",
+    technologies: ["HTML", "CSS", "JavaScript","Three.js"],
+  },
+];
+
+export const CONTACT = {
+  address: "Lucknow, Uttar Pradesh, India ",
+  phoneNo: "+91 705 439 6198",
+  email: "maurya.shubham@0221@gmail.com",
+};
